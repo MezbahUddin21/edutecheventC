98b24b66f7ae499a1465e3fdf4ebea320a128a72
Mezbah <mezbah.sc@gmail.com>
2026-05-22 00:14:26 +0600

    Initialize clean rebuild structure

--- app/Providers.js
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import {SessionProvider} from "next-auth/react";
-import { Children } from "react";
-
-export const AuthProvider=({children})=>{
-    return <SessionProvider>{children}</SessionProvider>
-}
